2314a1d2f74ee05ba486877f07954d61dcfa2a02
Travis Holton <travis@catalyst.net.nz>
2013-05-15 17:49:12 +1200

    added some necessary stuff to cocktail fu

--- lib/CocktailFu.pm
+++ lib/CocktailFu.pm
@@ -1,18 +1,50 @@
 package CocktailFu;
 use Mojo::Base 'Mojolicious';
 
+use File::Basename 'dirname';
+use File::Spec::Functions 'catdir';
+use Time::HiRes qw/time/;
+
+use CocktailFu::Schema;
+
+has schema => sub {
+    my $self   = shift;
+    my $config = $self->app->config;
+    ### config : $config
+    my $schema_config = $config->{'CocktailFu::Schema'};
+    my $connect_info  = $schema_config->{connect_info};
+    ### connect info: $connect_info
+    my ( $dsn, $user, $password ) = @{$connect_info}{qw/dsn user password/};
+    my $dbh = CocktailFu::Schema->connect( $dsn, $user, $password );
+    return $dbh;
+};
+
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+    my $self = shift;
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
+    $self->home->parse( catdir( dirname(__FILE__), 'CocktailFu' ) );
+    $self->static->paths->[0]   = $self->home->rel_dir('public');
+    $self->renderer->paths->[0] = $self->home->rel_dir('templates');
 
-  # Router
-  my $r = $self->routes;
+    #if ( $self->mode eq 'production' ) {
+    #$self->log->path('/home/cocktail_fu/log/vindaloo.log');
+    #}
 
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
+    my $config = $self->plugin('Config');
+
+    # Documentation browser under "/perldoc"
+    $self->plugin('PODRenderer');
+    $self->helper(
+        db => sub {
+            my $app = shift;
+            $self->app->schema;
+        }
+    );
+
+    # Router
+    my $r = $self->routes;
+
 }
 
 1;
