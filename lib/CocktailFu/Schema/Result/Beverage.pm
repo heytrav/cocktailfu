977cfd468d92b312926f4cf808f0803427e60001
Travis Holton <travis@catalyst.net.nz>
2013-06-02 19:34:50 +1200

    work on talk

--- lib/CocktailFu/Schema/Result/Beverage.pm
+++ lib/CocktailFu/Schema/Result/Beverage.pm
@@ -7,13 +7,13 @@ BEGIN { extends 'DBIx::Class::Core' }
 __PACKAGE__->table('beverages');
 __PACKAGE__->add_columns(qw/id name description/);
 __PACKAGE__->set_primary_key('id');
-__PACKAGE__->add_unique_constraint([qw/name/]);
+__PACKAGE__->add_unique_constraint( [qw/name/] );
 
 __PACKAGE__->has_many(
     recipes => 'Recipe' => { 'foreign.beverage' => 'self.id' } );
 
 __PACKAGE__->has_one(
-    instructions => Instruction => { 'foreign.beverage' => 'self.id' } );
+    instruction => Instruction => { 'foreign.beverage' => 'self.id' } );
 
 __PACKAGE__->many_to_many( ingredients => recipes => 'ingredient' );
 
